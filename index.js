30410ddb1fc30fa6cab0530412e68441472c1e78
Ayushsingh07 <nqu7069@gmail.com>
2023-02-10 21:54:30 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -10,13 +10,20 @@ const accountSid = 'AC42adc57b1e7641bfa5209041f877ce96';
 const authToken = '5a01b9256cb009d4c222b0893786159f';
 const client = new twilio(accountSid, authToken);
 
+
+app.get("",(req,res)=>{
+    console.log("hewllo")
+    res.send("hello")
+
+})
+
 app.post('/incoing', (req, res) => {
   const message = req.body.Body;
   console.log(`Received message: ${message}`);
   
   res.send(`<Response><Message>Thanks for your message: ${message}</Message></Response>`);
 });
-
-app.listen(3000, () => {
+const PORT = process.env.PORT||3000
+app.listen(PORT, () => {
   console.log('WhatsApp bot listening on port 3000!');
 });
